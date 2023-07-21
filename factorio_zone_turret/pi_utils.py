c3eee0c887d22b7d1364c8e42972040c42af30cf
Igor Zamyslov <igor.zamyslov@cloudflight.io>
2023-07-21 15:13:50 +0200

    Remove LED mock

--- factorio_zone_turret/pi_utils.py
+++ factorio_zone_turret/pi_utils.py
@@ -1,36 +1,9 @@
-from gpiozero import BadPinFactory, RGBLED
+from gpiozero import Button, RGBLED
 
 from factorio_zone_turret import colors
 
-
-def _get_mock_led():
-    """ Returns mock instance for running the project on a PC and not on Pi """
-    from dataclasses import dataclass
-
-    @dataclass
-    class MockLED:
-        _color: colors.Color = None
-
-        @property
-        def color(self):
-            return self._color
-
-        @color.setter
-        def color(self, value):
-            print("Setting LED color to", value)
-            self._color = value
-
-        def pulse(self, on_color, **kwargs):
-            print("Pulsing LED with color", on_color)
-
-    return MockLED()
-
-
-try:
-    led = RGBLED(17, 27, 22)
-except BadPinFactory:
-    print("Error: LED not available")
-    led = _get_mock_led()
+led = RGBLED(17, 27, 22)
+button = Button(2)
 
 
 def change_led_color(color: colors.Color):
