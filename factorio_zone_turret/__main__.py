fc967435b9c67662c2d382ed1ffde0210bae9c0a
Igor Zamyslov <zamyslovigor@gmail.com>
2023-07-29 19:10:15 +0200

    Fix: Reinitialize client on connection error for correct color handling

--- factorio_zone_turret/__main__.py
+++ factorio_zone_turret/__main__.py
@@ -9,36 +9,46 @@ from factorio_zone_turret.handlers import create_button_press_handler, \
     create_server_status_change_handler, handle_hosting_started, handle_players_count
 from factorio_zone_turret.pi_utils import button, pulse_led
 
-pulse_led(colors.DARK_WHITE, colors.WHITE)
-client = FZClient(token=config.FZ_TOKEN)
 
-# Create button handlers
-button.when_activated = create_button_press_handler(client)
+def init_client():
+    logging.info("Initializing client")
+    pulse_led(colors.DARK_WHITE, colors.WHITE)
+    client = FZClient(token=config.FZ_TOKEN)
 
-# Create server status handlers
-handle_server_status_change = create_server_status_change_handler(client)
-client.add_message_listener(handle_server_status_change)
-client.add_logs_listener(handle_players_count)
-client.add_logs_listener(handle_hosting_started)
+    # Create button handlers
+    button.when_activated = create_button_press_handler(client)
 
-class bcolors:  
-    ORANGE =  '\033[38;5;202m'
-    ENDC      = '\033[0m'
+    # Create server status handlers
+    handle_server_status_change = create_server_status_change_handler(client)
+    client.add_message_listener(handle_server_status_change)
+    client.add_logs_listener(handle_players_count)
+    client.add_logs_listener(handle_hosting_started)
+    logging.info("Client ready")
+    return client
 
-logo = """  ______ ______  _______                  _   
- |  ____|___  / |__   __|                | |  
- | |__     / /     | |_   _ _ __ _ __ ___| |_ 
- |  __|   / /      | | | | | '__| '__/ _ \ __|
- | |     / /__     | | |_| | |  | | |  __/ |_ 
- |_|    /_____|    |_|\__,_|_|  |_|  \___|\__|
-"""
-print(f"{bcolors.ORANGE}{logo}{bcolors.ENDC}")
 
-logging.info("Client ready")
+def print_logo():
+    class bcolors:  
+        ORANGE =  '\033[38;5;202m'
+        ENDC      = '\033[0m'
+
+    logo = """
+     ______ ______  _______                  _   
+    |  ____|___  / |__   __|                | |  
+    | |__     / /     | |_   _ _ __ _ __ ___| |_ 
+    |  __|   / /      | | | | | '__| '__/ _ \ __|
+    | |     / /__     | | |_| | |  | | |  __/ |_ 
+    |_|    /_____|    |_|\__,_|_|  |_|  \___|\__|
+    """
+    print(f"{bcolors.ORANGE}{logo}{bcolors.ENDC}")
+
+
+print_logo()
+client = init_client()
 while True:
     try:
         asyncio.run(client.connect())
     except Exception as error:
         logging.warning("Reconnecting due to the following exception: %s", error, exc_info=True)
-        pulse_led(colors.DARK_WHITE, colors.WHITE)
         sleep(5)
+        client = init_client()
