7c8b88d585916efeb4aa486eadae4cabdfd7a366
Igor Zamyslov <igor.zamyslov@cloudflight.io>
2023-07-31 17:40:44 +0200

    Implement a FZT server to do a proper reset

--- factorio_zone_turret/__main__.py
+++ factorio_zone_turret/__main__.py
@@ -1,36 +1,14 @@
-import asyncio
 import logging
 from time import sleep
 
-from factorio_zone_turret import colors
-from factorio_zone_turret.config import config
-from factorio_zone_turret.fz_manager.fz_manager.factorio_zone_api import FZClient
-from factorio_zone_turret.handlers import create_button_press_handler, \
-    create_server_status_change_handler, handle_hosting_started, handle_players_count
-from factorio_zone_turret.pi_utils import button, pulse_led
+from factorio_zone_turret.server import FZTurretServer
 
 
-def init_client():
-    logging.info("Initializing client")
-    pulse_led(colors.DARK_WHITE, colors.WHITE)
-    client = FZClient(token=config.FZ_TOKEN)
-
-    # Create button handlers
-    button.when_activated = create_button_press_handler(client)
-
-    # Create server status handlers
-    handle_server_status_change = create_server_status_change_handler(client)
-    client.add_message_listener(handle_server_status_change)
-    client.add_logs_listener(handle_players_count)
-    client.add_logs_listener(handle_hosting_started)
-    logging.info("Client ready")
-    return client
-
-
+# Logo
 def print_logo():
-    class bcolors:  
-        ORANGE =  '\033[38;5;202m'
-        ENDC      = '\033[0m'
+    class bcolors:
+        ORANGE = '\033[38;5;202m'
+        ENDC = '\033[0m'
 
     logo = """
      ______ ______  _______                  _   
@@ -44,11 +22,13 @@ def print_logo():
 
 
 print_logo()
-client = init_client()
+
+# Main loop
+server = FZTurretServer()
 while True:
     try:
-        asyncio.run(client.connect())
+        server.start()
     except Exception as error:
         logging.warning("Reconnecting due to the following exception: %s", error, exc_info=True)
         sleep(5)
-        client = init_client()
+        server.reset()
